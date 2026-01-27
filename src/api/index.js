3a9f8d1e3da011228439ede2b3ee9a47faca31b7
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2026-01-27 17:15:51 +0100

    index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,15 @@
 const baseURL = 'https://morpion-api.edu.netlor.fr'
 const moveWsURL = 'https://morpion-api.edu.netlor.fr/websockets'
-//key API Mattéo : HLTkU)Mt#9wQ
+
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `key=${import.meta.env.VITE_API_KEY}`
+  }
+})
+
+export { moveWsURL }
+export default instance
